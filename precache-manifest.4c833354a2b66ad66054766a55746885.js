a6f4b0f4a0503eb1e19ce25913f40f8b149d008f
Anish <lushteanish@gmail.com>
2020-05-27 10:50:09 +0530

    as

---
+++ precache-manifest.4c833354a2b66ad66054766a55746885.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34658430d8bfecfef081c26b3fc0c822",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24759ab83de46ff605bb",
+    "url": "/static/css/main.c7509e07.chunk.css"
+  },
+  {
+    "revision": "6c013269363f440de59b",
+    "url": "/static/js/2.e21cadb2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e21cadb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24759ab83de46ff605bb",
+    "url": "/static/js/main.cabd5450.chunk.js"
+  },
+  {
+    "revision": "7607ea4b3c3a967d5a04",
+    "url": "/static/js/runtime-main.7ca5017b.js"
+  },
+  {
+    "revision": "a585f0be0d7d463f65a386d89624afde",
+    "url": "/static/media/attendance.a585f0be.png"
+  },
+  {
+    "revision": "4d0b85015ee6999cc417fe0d55cb1e43",
+    "url": "/static/media/close.4d0b8501.svg"
+  },
+  {
+    "revision": "629788f31c3a64e09219fbc850d31ae7",
+    "url": "/static/media/dateScroll.629788f3.svg"
+  },
+  {
+    "revision": "86564a764bbda7766858dc5d64e28261",
+    "url": "/static/media/download.86564a76.svg"
+  },
+  {
+    "revision": "56a7fdc10428d0fcb1c86f216bdc2160",
+    "url": "/static/media/goal-1.56a7fdc1.svg"
+  },
+  {
+    "revision": "687c18dc5f840535f9cc26c8336a42a7",
+    "url": "/static/media/goal-2.687c18dc.svg"
+  },
+  {
+    "revision": "39d8019c1b0e7327c02e6459fb7dd7c7",
+    "url": "/static/media/goal-3.39d8019c.svg"
+  },
+  {
+    "revision": "3a3ec0728b77f6fed779eade56cb9abf",
+    "url": "/static/media/homeWorkCompleted.3a3ec072.svg"
+  },
+  {
+    "revision": "f51cf98ad8008c179d1cc2ffb6736ca6",
+    "url": "/static/media/homework.f51cf98a.svg"
+  },
+  {
+    "revision": "e781e7e2a9648151119e7bf012438607",
+    "url": "/static/media/homeworkNav.e781e7e2.svg"
+  },
+  {
+    "revision": "822f8717bd6858c92278cd9cef56a7aa",
+    "url": "/static/media/liveClass.822f8717.svg"
+  },
+  {
+    "revision": "9be991a021003a81ffdb588276a8897d",
+    "url": "/static/media/liveClassNav.9be991a0.svg"
+  },
+  {
+    "revision": "5b07723535fc466c975d016220476cc1",
+    "url": "/static/media/profile.5b077235.png"
+  },
+  {
+    "revision": "b19739eaaac460cb3b404cf6a97eba01",
+    "url": "/static/media/profileCard.b19739ea.svg"
+  },
+  {
+    "revision": "fb646aa7921eb8cb1ff72d99d2d6074d",
+    "url": "/static/media/progressComplete.fb646aa7.svg"
+  },
+  {
+    "revision": "d2109bf435bca88ed3f1b508ee5c79ea",
+    "url": "/static/media/progressIncomplete.d2109bf4.svg"
+  },
+  {
+    "revision": "f1733147fb4ab183a4755d1217de1884",
+    "url": "/static/media/quiz.f1733147.svg"
+  },
+  {
+    "revision": "b84fb192a10f8241b120210164f18a2a",
+    "url": "/static/media/quizCompleted.b84fb192.svg"
+  },
+  {
+    "revision": "0bdaee03523a676f8f0608443c889dab",
+    "url": "/static/media/quiznav.0bdaee03.svg"
+  },
+  {
+    "revision": "ea7f4f0006efbb08f402d39b9904d85f",
+    "url": "/static/media/sa-tile.ea7f4f00.svg"
+  },
+  {
+    "revision": "4ae37036c76b920e2d0a2c1cba93ebd6",
+    "url": "/static/media/start.4ae37036.svg"
+  },
+  {
+    "revision": "cbf7b2bae2d4ffc8b25cc4e57d7a0689",
+    "url": "/static/media/tag.cbf7b2ba.svg"
+  },
+  {
+    "revision": "757db30a9b1d388ecb778fc9278ca258",
+    "url": "/static/media/topnav.757db30a.svg"
+  }
+]);
